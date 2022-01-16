a1c10f108be73f38734b9508ef7337f35d097524
Gabriel Santos <gabriel.santos.iami@gmail.com>
2022-01-16 00:34:25 -0300

    Criação de um package

---
+++ django-polls/polls/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ['pub_date']
+
+    search_fields = ['question_text']
+
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Campos de data', {'fields': ['pub_date']}),
+    ]
+
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+admin.site.register(Question, QuestionAdmin)
